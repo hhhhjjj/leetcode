6a08da82fd4f6461e8bcb62859035fbf1341cf37
HeJames <48619125+hhhhjjj@users.noreply.github.com>
2020-02-02 22:41:44 +0800

    链表

--- 分裂二叉树的最大乘积.py
+++ 分裂二叉树的最大乘积.py
@@ -8,7 +8,19 @@ class TreeNode:
 
 class Solution:
     def maxProduct(self, root):
-        # 想要乘积尽可能大，两边最好相等
-        sum_left = 0
-        sum_right = 0
+        # 想要乘积尽可能大，两边越接近越大
+
+        def get_sum(root):
+            res = root.val
+            if root.left:
+                res += get_sum(root.left)
+            if root.right:
+                res += get_sum(root.right)
+            return res
+
+        sum_left = root.val + get_sum(root.left)
+        sum_right = get_sum(root.right)
+        res = sum_left * sum_right
+        res_left = (sum_left - root.val) * (sum_right + root.val)
+
         
