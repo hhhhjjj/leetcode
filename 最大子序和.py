ac73de4d2143685b80c494f0fd3ba982bcbd98aa
HeJames <48619125+hhhhjjj@users.noreply.github.com>
2020-03-07 23:14:16 +0800

    动态规划

---
+++ 最大子序和.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+class Solution:
+    def maxSubArray(self, nums: [int]) -> int:
+        # 动态规划或者贪心算法或者分治法
+        # 我那滑动窗口写的贼垃圾
+        n = len(nums)
+        max_sum = nums[0]
+        for i in range(1, n):
+            # 不需要两个指针，在这就是保留前面的数据，其实是动态规划
+            if nums[i - 1] > 0:
+                nums[i] += nums[i - 1]
+                # 贪心算法也差不多
+            max_sum = max(nums[i], max_sum)
+        return max_sum
+
+print(Solution().maxSubArray([1,2,-1,-2,2,1,-2,1,4,-5,4]))
