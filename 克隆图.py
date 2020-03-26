c7f790362f8df067b31e87bc845d1acc4d4abadc
HeJames <48619125+hhhhjjj@users.noreply.github.com>
2020-03-26 19:50:47 +0800

    图

---
+++ 克隆图.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+class Node:
+    def __init__(self, val = 0, neighbors = []):
+        self.val = val
+        self.neighbors = neighbors
+
+
+class Solution(object):
+
+    def __init__(self):
+        # 访问过的就不访问了
+        self.visited = {}
+
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        if not node:
+            return node
+
+        # If the node was already visited before.
+        # Return the clone from the visited dictionary.
+        if node in self.visited:
+            return self.visited[node]
+
+        # Create a clone for the given node.
+        # Note that we don't have cloned neighbors as of now, hence [].
+        clone_node = Node(node.val, [])
+        # 其实就是在这重新构造个节点罢了
+
+        # The key is original node and value being the clone node.
+        self.visited[node] = clone_node
+
+        # Iterate through the neighbors to generate their clones
+        # and prepare a list of cloned neighbors to be added to the cloned node.
+        if node.neighbors:
+            clone_node.neighbors = [self.cloneGraph(n) for n in node.neighbors]
+
+        return clone_node
