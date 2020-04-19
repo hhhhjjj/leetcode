f9dc6dcadb0aeceaa0830b6311c36b1952067d47
HeJames <1094231447@qq.com>
2020-04-19 10:28:40 +0800

    个人赛

--- 剧情触发时间.py
+++ 剧情触发时间.py
@@ -1,37 +1,26 @@
 # -*- coding: utf-8 -*-
 class Solution:
     def getTriggerTime(self, increase: [[int]], requirements: [[int]]) -> [int]:
-        increase = [[0, 0, 0]] + increase
-        length = len(requirements)
-        length_i = len(increase)
-        res = [-1] * length
-        # 这个题感觉就根据c来构建个索引把
-        c_dict = {}
-        ruler = []
-        for i in range(length):
-            if c_dict.get(requirements[i][0]):
-                c_dict[requirements[i][0]].append(i)
-            else:
-                c_dict[requirements[i][0]] = [i]
-                ruler.append(requirements[i][0])
-        ruler.sort()
-        C = 0
-        R = 0
-        H = 0
-        for inc in range(length_i):
-            C += increase[inc][0]
-            R += increase[inc][1]
-            H += increase[inc][2]
-            for r in ruler:
-                if r <= C:
-                    del_list= []
-                    for i in c_dict[r]:
-                        if requirements[i][0] <= C and requirements[i][1] <= R and requirements[i][2] <= H:
-                            res[i] = inc
-                            del_list.append(i)
-                    for i in del_list:
-                        c_dict[r].remove(i)
+        # 这里用一个前缀和不行，要用三个记录才比较快
+        n, r = len(increase), len(requirements)
+        preSum_c, preSum_r, preSum_h = [0] * (n + 1), [0] * (n + 1), [0] * (n + 1)
+        res = [-1] * r
+
+        for i in range(n):
+            preSum_c[i + 1] = preSum_c[i] + increase[i][0]
+            preSum_r[i + 1] = preSum_r[i] + increase[i][1]
+            preSum_h[i + 1] = preSum_h[i] + increase[i][2]
+        for i in range(r):
+            left, right = 0, n
+            while left <= right:
+                # 这里用了个二分查找
+                mid = left + (right - left) // 2
+                if preSum_c[mid] >= requirements[i][0] and preSum_r[mid] >= requirements[i][1] and preSum_h[mid] >= \
+                        requirements[i][2]:
+                    res[i] = mid
+                    right = mid - 1
+                else:
+                    left = mid + 1
         return res
-# 还是超时了
 
 print(Solution().getTriggerTime(increase = [[0,4,5],[4,8,8],[8,6,1],[10,10,0]],requirements = [[12,11,16],[20,2,6],[9,2,6],[10,18,3],[8,14,9]]))
--- 最小跳跃次数.py
+++ 最小跳跃次数.py
@@ -2,13 +2,35 @@
 class Solution:
     def minJump(self, jump: [int]) -> int:
         # 之前的题目是问能不能跳出去，现在这个就要求最小的跳跃次数
-        n = len(jump)
-        step_list = [1] * n
-        max_num = 0
-        for i in range(n):
-            if i + jump[i] >= n:
-                return step_list[i]
+        # 还是bfs，然后记录状态
+        visit = [0 for _ in range(len(jump))]
+        # 队列，记录一个元组：（当前位置，当前jump次数）
+        # 初始状态为（0，0）
+        q = [(0, 0)]
+        visit[0] = 1
+        # 该参数记录已访问过的最大index，该位置左边的弹簧均已被遍历
+        # 对于i<=max_left_idx, visit[i]==1。
+        max_left_idx = 0
+        # BFS遍历
+        while q:
+            cur_idx, count = q.pop(0)
+            # 情况1: 向右跳
+            right_idx = cur_idx + jump[cur_idx]
+            # 小球弹出机器只可能发生在向右跳的情况，小球弹出时返回答案
+            if right_idx >= len(jump):
+                return count + 1
             else:
-                max_num = max(max_num, i + jump[i])
+                if not visit[right_idx]:
+                    q.append((right_idx, count + 1))
+                    visit[right_idx] = 1
+            # 情况2: 向左跳
+            # max_left_idx左边的弹簧已经遍历过，不需要再访问，直接从max_left_idx+1开始遍历至cur_idx
+            # 如果不设置该参数，需要从0遍历置cur_idx，造成TLE错误
+            for i in range(max_left_idx + 1, cur_idx):
+                if not visit[i]:
+                    q.append((i, count + 1))
+                    visit[i] = 1
+            # 更新max_left_idx的值，即当前cur_idx左边的弹簧已经遍历过
+            max_left_idx = cur_idx
 
 
