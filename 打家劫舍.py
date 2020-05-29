6cd61ebac8aa04991bd618939f42e54bfb627cc1
HeJames <48619125+hhhhjjj@users.noreply.github.com>
2020-05-29 09:13:15 +0800

    Add files via upload

---
+++ 打家劫舍.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+class Solution:
+    def rob(self, nums):
+        # 这个应该是动态规划了
+        if not nums:
+            return 0
+
+        size = len(nums)
+        if size == 1:
+            return nums[0]
+
+        first, second = nums[0], max(nums[0], nums[1])
+        for i in range(2, size):
+            first, second = second, max(first + nums[i], second)
+
+        return second
